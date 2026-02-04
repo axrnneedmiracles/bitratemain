62ef3a43d38b65401bfc3a920afa169d12ca9837
Aryan Vats <aryanvats220606@gmail.com>
2026-02-04 23:19:55 +0000

    src/components/contact-form.tsx (89:27) @ onSubmit
      87 |
      88 |     t

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -88,7 +88,6 @@ export function ContactForm() {
     try {
         const res = await fetch("https://script.google.com/macros/s/AKfycbxR_6TU-bFVnengiOFYY-GQVOyxgRxuAEcW2PCivPmT-zqo1r-7DAHEpsPTXHKFX3jG0g/exec", {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload),
         });
 
