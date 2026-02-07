14b830dcce40f0b55936b4712e6bcc7e16cc53b6
Aryan Vats <aryanvats220606@gmail.com>
2026-02-07 15:52:15 +0000

    reduce the division space for buttons its too big

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex-grow flex flex-col items-center pt-36">
+    <div className="flex-grow flex flex-col items-center pt-20">
       <TextType
         as="h1"
         text={['WELCOME TO BITRATE STUDIO']}
@@ -28,7 +28,7 @@ export default function Home() {
         loop={false}
         className="text-4xl md:text-6xl font-headline text-center font-bold"
       />
-      <div className="inline-flex gap-4 mt-12">
+      <div className="inline-flex gap-4 mt-4 mb-2">
         <Button asChild size="lg">
           <Link href="/portfolio">PORTFOLIO</Link>
         </Button>
@@ -36,7 +36,7 @@ export default function Home() {
           <Link href="/contact">CONTACT US</Link>
         </Button>
       </div>
-      <div className="w-full h-[600px] relative mt-12">
+      <div className="w-full h-[400px] relative">
         <CircularGallery items={galleryItems} bend={1} textColor="#ffffff" borderRadius={0.05} scrollEase={0.05} scrollSpeed={0.5} autoScrollSpeed={0.05} />
       </div>
     </div>
