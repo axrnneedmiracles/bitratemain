09134ea57467a8d43a54d257d0ef685eb792d78f
Aryan Vats <aryanvats220606@gmail.com>
2026-02-07 15:43:09 +0000

    reduce size of circular gallery very little bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex-grow flex flex-col items-center pt-24">
+    <div className="flex-grow flex flex-col items-center pt-36">
       <TextType
         as="h1"
         text={['WELCOME TO BITRATE STUDIO']}
@@ -28,7 +28,7 @@ export default function Home() {
         loop={false}
         className="text-4xl md:text-6xl font-headline text-center font-bold"
       />
-      <div className="flex gap-4 mt-12">
+      <div className="inline-flex gap-4 mt-16">
         <Button asChild size="lg">
           <Link href="/portfolio">PORTFOLIO</Link>
         </Button>
@@ -36,7 +36,7 @@ export default function Home() {
           <Link href="/contact">CONTACT US</Link>
         </Button>
       </div>
-      <div className="w-full h-[600px] relative mt-12">
+      <div className="w-full h-[680px] relative mt-16">
         <CircularGallery items={galleryItems} bend={1} textColor="#ffffff" borderRadius={0.05} scrollEase={0.05} scrollSpeed={0.5} autoScrollSpeed={0.05} />
       </div>
     </div>
