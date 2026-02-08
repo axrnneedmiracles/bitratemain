48fc2e608486e8b1028bf498d303c46f401edcfa
Aryan Vats <aryanvats220606@gmail.com>
2026-02-08 21:37:13 +0000

    reverse this update, the background is completely gone reverse it now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import StaggeredMenu from '@/components/staggered-menu';
 import LayoutClient from '@/components/layout-client';
+import Hyperspeed from '@/components/hyperspeed';
+import SplashCursor from '@/components/splash-cursor';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -44,6 +46,49 @@ export default function RootLayout({
               items={menuItems}
               socialItems={socialItems}
             />
+           <SplashCursor
+            DENSITY_DISSIPATION={4}
+            SPLAT_RADIUS={0.1}
+          />
+           <div className="fixed top-0 left-0 -z-10 h-full w-full">
+            <Hyperspeed
+              effectOptions={{
+                distortion: 'turbulentDistortion',
+                length: 400,
+                roadWidth: 10,
+                islandWidth: 2,
+                lanesPerRoad: 3,
+                fov: 90,
+                fovSpeedUp: 150,
+                speedUp: 2,
+                carLightsFade: 0.4,
+                totalSideLightSticks: 5,
+                lightPairsPerRoadWay: 10,
+                shoulderLinesWidthPercentage: 0.05,
+                brokenLinesWidthPercentage: 0.1,
+                brokenLinesLengthPercentage: 0.5,
+                lightStickWidth: [0.12, 0.5],
+                lightStickHeight: [1.3, 1.7],
+                movingAwaySpeed: [60, 80],
+                movingCloserSpeed: [-120, -160],
+                carLightsLength: [12, 80],
+                carLightsRadius: [0.05, 0.14],
+                carWidthPercentage: [0.3, 0.5],
+                carShiftX: [-0.8, 0.8],
+                carFloorSeparation: [0, 5],
+                colors: {
+                  roadColor: 526344,
+                  islandColor: 657930,
+                  background: 0,
+                  shoulderLines: 1250072,
+                  brokenLines: 1250072,
+                  leftCars: [14177983, 6770850, 12732332],
+                  rightCars: [242627, 941733, 3294549],
+                  sticks: 242627,
+                },
+              }}
+            />
+          </div>
 
           <div className="relative z-0 flex flex-col min-h-screen">
             <main className="flex-grow">{children}</main>
